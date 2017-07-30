d6a8febe1d49b1c7d7a86ba6c4b0936dab874097
Santiago Fernandez <sfernandez@matriz.io>
2017-07-30 15:14:41 -0300

    Session worker tests

--- test/ex_fix_test.exs
+++ test/ex_fix_test.exs
@@ -1,9 +1,12 @@
 defmodule ExFix.ExFixTest do
   use ExUnit.Case
+  alias ExFix.SessionRegistry
+  alias ExFix.Parser
+  alias ExFix.DefaultDictionary
 
   defmodule TestTransport do
     def connect(_host, _port, options) do
-      {:ok, options[:pid]}
+      {:ok, options[:test_pid]}
     end
     def send(conn, data) do
       Process.send(conn, {:data, data}, [])
@@ -27,8 +30,11 @@ defmodule ExFix.ExFixTest do
 
   test "Session initiator test" do
     ExFix.start_session_initiator("session1", "SENDER", "TARGET", TestApplication,
-      transport_mod: TestTransport, transport_options: [pid: self()])
-    assert_receive {:data, _}
-    assert ExFix.SessionRegistry.get_status == %{"session1" => :connecting}
+      transport_mod: TestTransport, transport_options: [test_pid: self()])
+    assert_receive {:data, logon_msg}
+    assert SessionRegistry.get_status == %{"session1" => :connecting}
+    assert "8=FIXT.1.1" <> _ = logon_msg
+    msg = Parser.parse(logon_msg, DefaultDictionary, 1)
+    assert msg.valid
   end
 end
