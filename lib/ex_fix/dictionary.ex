32d2f80c99ade2f533c1677ab04bc76e60677125
Santiago Fernandez <sfernandez@matriz.io>
2017-08-01 14:40:14 -0300

    Fix dialyzer error

--- lib/ex_fix/dictionary.ex
+++ lib/ex_fix/dictionary.ex
@@ -7,7 +7,6 @@ defmodule ExFix.Dictionary do
   @type tag_info() :: {tag_name :: String.t, tag_type :: tag_type()}
   @type subject_tag() :: String.t | {String.t, String.t} | nil
 
-  @callback msg_type(name :: String.t) :: String.t
   @callback tag_info(tag :: String.t) :: tag_info()
   @callback subject(msg_type :: String.t) :: subject_tag()
 end
