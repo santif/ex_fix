d51d7cc9cdd437e1f5c580bac3e7b4388c9c91d7
Santiago Fernandez <sfernandez@matriz.io>
2017-08-09 20:23:44 -0300

    Change timer name in test

--- test/session_timer_test.exs
+++ test/session_timer_test.exs
@@ -4,8 +4,8 @@ defmodule ExFix.SessionTimerTest do
   alias ExFix.SessionTimer
 
   test "SessionTimer test" do
-    timer = SessionTimer.setup_timer("timer1", 50)
+    timer = SessionTimer.setup_timer(:timer1, 50)
     send(timer, :msg)
-    assert_receive {:timeout, "timer1"}, 150
+    assert_receive {:timeout, :timer1}, 100
   end
 end
