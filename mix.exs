461efe3072d512a4ebde99a8c533f9fb11e5aa5a
Santiago Fernandez <santif@gmail.com>
2025-06-19 19:56:01 -0300

    Remove excoveralls and use built-in coverage

--- mix.exs
+++ mix.exs
@@ -10,13 +10,7 @@ defmodule ExFix.Mixfile do
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test
-      ],
+      test_coverage: [summary: [threshold: 0]],
       deps: deps()
     ]
   end
@@ -44,7 +38,6 @@ defmodule ExFix.Mixfile do
     [
       {:benchfella, "~> 0.3.5", only: :dev},
       {:ex_doc, "~> 0.30", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.18", only: :test},
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
