463639adc882e434ae4d01052163e15112169a7a
Santiago Fernandez <santif@gmail.com>
2025-06-19 21:28:35 -0300

    Fix coverage badge generation

--- scripts/generate_coverage_badge.sh
+++ scripts/generate_coverage_badge.sh
@@ -7,6 +7,7 @@ coverage=${coverage:-0}
 
 anybadge --value="${coverage}" --label=coverage \
   --file=coverage.svg \
+  --overwrite \
   0=red 80=yellow 90=green >/dev/null
 
 echo "Generated coverage.svg with ${coverage}% coverage"
